79c497e8420e2ef265a532ed774494eca9508903
UserIvanTs <115897732+UserIvanTs@users.noreply.github.com>
2023-01-07 17:28:04 +0300

    Update index.test.js

--- index.test.js
+++ index.test.js
@@ -1,10 +1,10 @@
-const  match  =  require ( "./index" ) ;
-test ( "hEllO=HELLo" ,  ( )  =>  {
-  ожидать ( соответствие ( "привет" ,  "привет" ) ) . быть ( правда ) ;
-} ) ;
-test ( "Hel1o != привет" ,  ( )  =>  {
-  ожидать ( соответствие ( "Hel1o" ,  "привет" ) ) . быть ( ложь ) ;
-} ) ;
-тест ( "ПРИВЕТ != 1234" ,  ( )  =>  {
-  ожидать ( совпадение ( "ПРИВЕТ" ,  "1234" ) ) . быть ( ложь ) ;
-} ) ;
+const match = require("./index");
+test("hEllO=HELLo", () => {
+  expect(match("hEllO", "hellO")).toBe(true);
+});
+test("Hel1o != hello", () => {
+  expect(match("Hel1o", "hello")).toBe(false);
+});
+test("HELLO != 1234", () => {
+  expect(match("HELLO", "1234")).toBe(false);
+});
